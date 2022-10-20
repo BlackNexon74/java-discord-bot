96039348957b76775405402bdd64ff2f4476b2a5
n.breton <nexon70000@gmail.com>
2022-10-20 15:00:01 +0200

    1.1 20/10/22

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,8 @@ import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+
+import java.util.ArrayList;
 import java.util.Random;
 
 import javax.security.auth.login.LoginException;
@@ -35,7 +37,7 @@ public class Main extends ListenerAdapter {
                     event.getChannel().sendMessage("T'es génant").queue();
                 }
                 else{
-                    event.getChannel().sendMessage(Gifs.cringe()).queue();
+                    event.getChannel().sendMessage(cringe()).queue();
                 }
             }
         }
@@ -48,6 +50,37 @@ public class Main extends ListenerAdapter {
                 event.getChannel().sendMessage("Tu es cringe à dire ''qui ?''").queue();
             }
         }
+        else if(event.getMessage().getContentRaw().toLowerCase().contains("!ratio")){
+            int a = getRandomNumberInRange(1, 2);
+            if(a==1){
+                event.getChannel().sendMessage("Nan ta gueule").queue();
+            }
+            else {
+                String[] args = event.getMessage().getContentRaw().split(" ");
+                event.getChannel().sendMessage(event.getMessage().getAuthor() + "ratio" + args[1] + ratio()).queue();
+                event.getMessage().delete().queue();
+            }
+        }
+    }
+
+    public String ratio(){
+        String res;
+        ArrayList<String> ratio = new ArrayList<>();
+        ratio.add("la salope");
+        ratio.add("la petite pute");
+        ratio.add("la merde");
+        ratio.add("le sous humain");
+        return ratio.get(getRandomNumberInRange(0,3));
+    }
+
+    public String cringe(){
+        ArrayList<String> gif = new ArrayList<>();
+        gif.add("https://tenor.com/view/dies-of-cringe-cringe-gif-20747133");
+        gif.add("https://tenor.com/view/cringe-youre-cringe-dies-of-cringe-cringe-af-gif-25319855");
+        gif.add("https://tenor.com/view/cringe-gif-23594630");
+        gif.add("https://i1.sndcdn.com/artworks-lFHmV922CalFXzTx-1BzOMg-t500x500.jpg");
+        gif.add("https://ih1.redbubble.net/image.713481123.5957/flat,800x800,075,f.u2.jpg");
+        return gif.get(getRandomNumberInRange(0,4));
     }
 
     private static int getRandomNumberInRange(int min, int max) {
